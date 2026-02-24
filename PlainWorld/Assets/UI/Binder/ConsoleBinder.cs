9c0776aa053381ec38845655353807c1d705f64a
Long Dong <longdong32120@gmail.com>
2026-02-24 13:26:20 +0700

    update: styling

--- PlainWorld/Assets/UI/Binder/ConsoleBinder.cs
+++ PlainWorld/Assets/UI/Binder/ConsoleBinder.cs
@@ -4,51 +4,7 @@ using Assets.Utility;
 using System.Collections;
 using UnityEngine;
 
-public class ConsoleBinder : ComponentBinder
+public class ConsoleBinder //: ComponentBinder
 {
-    #region Attributes
-    [SerializeField]
-    private ConsoleView consoleView;
-    private ConsolePresenter consolePresenter;
 
-    private UIService uiService;
-    private ConsoleService consoleService;
-    #endregion
-
-    #region Properties
-    public override string StepName
-    {
-        get { return "HUD: Utils Console"; }
-    }
-    #endregion
-
-    #region Methods
-    public override IEnumerator BindAllServices()
-    {
-        yield return BindWhenReady<UIService>(ui =>
-        {
-            uiService = ui;
-        });
-
-        yield return BindWhenReady<ConsoleService>(console =>
-        {
-            consoleService = console;
-        });
-
-        // Resolve dependencies
-        consolePresenter = new ConsolePresenter(
-            uiService,
-            consoleService,
-            consoleView);
-
-        GameLogger.Info(
-            Channel.System,
-            "HUD: Utils Console components binded successfully");
-    }
-
-    private void OnDestroy()
-    {
-        consolePresenter?.Dispose();
-    }
-    #endregion
 }
